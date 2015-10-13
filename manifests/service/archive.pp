9c5ba4b0cac1e628a67449112cea71801409c1e0
Antoine Cotten <antoine@freeletics.com>
2015-10-13 11:31:51 +0200

    refactoring of forwarded variables

--- manifests/service/archive.pp
+++ manifests/service/archive.pp
@@ -34,18 +34,18 @@ class tomcat::service::archive {
     exec { "refresh_${service_name_real}":
       command     => '/usr/bin/systemctl daemon-reload',
       refreshonly => true,
-      subscribe   => File["${::tomcat::service_name_real} service unit"],
-      notify      => Service[$::tomcat::service_name_real]
+      subscribe   => File["${service_name_real} service unit"],
+      notify      => Service[$service_name_real]
     }
   } else { # Debian/Ubuntu, RHEL 6, SLES 11, ...
-    $start_command = "export CATALINA_BASE=${::tomcat::catalina_base_real}; /bin/su ${::tomcat::tomcat_user_real} -s /bin/bash -c '${::tomcat::service_start_real}'"
-    $stop_command = "export CATALINA_BASE=${::tomcat::catalina_base_real}; /bin/su ${::tomcat::tomcat_user_real} -s /bin/bash -c '${::tomcat::service_stop_real}'"
+    $start_command = "export CATALINA_BASE=${::tomcat::catalina_base_real}; /bin/su ${::tomcat::tomcat_user_real} -s /bin/bash -c '${service_start_real}'"
+    $stop_command = "export CATALINA_BASE=${::tomcat::catalina_base_real}; /bin/su ${::tomcat::tomcat_user_real} -s /bin/bash -c '${service_stop_real}'"
     $status_command = "/usr/bin/pgrep -d , -u ${::tomcat::tomcat_user_real} -G ${::tomcat::tomcat_group_real} -f Dcatalina.base=${::tomcat::catalina_base_real}"
 
     # create init script
-    file { "${::tomcat::service_name_real} service unit":
+    file { "${service_name_real} service unit":
       ensure  => present,
-      path    => "/etc/init.d/${::tomcat::service_name_real}",
+      path    => "/etc/init.d/${service_name_real}",
       owner   => 'root',
       group   => 'root',
       mode    => '0755',
@@ -53,9 +53,9 @@ class tomcat::service::archive {
     }
   }
 
-  service { $::tomcat::service_name_real:
+  service { $service_name_real:
     ensure  => $::tomcat::service_ensure,
     enable  => $::tomcat::service_enable,
-    require => File["${::tomcat::service_name_real} service unit"];
+    require => File["${service_name_real} service unit"];
   }
 }
