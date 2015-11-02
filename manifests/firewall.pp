75605de6588cb8f97a74d6786fc4afaf11d7d563
Frank Holtz <scitechfh@users.noreply.github.com>
2015-11-02 08:14:41 +0100

    Fix: Switch firewall from port to dport

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -9,7 +9,7 @@ class tomcat::firewall {
   # http connector
   if $::tomcat::http_connector {
     firewall { "${::tomcat::http_port} accept - tomcat":
-      port   => $::tomcat::http_port,
+      dport  => $::tomcat::http_port,
       proto  => 'tcp',
       action => 'accept'
     }
@@ -18,7 +18,7 @@ class tomcat::firewall {
   # ajp connector
   if $::tomcat::ajp_connector {
     firewall { "${::tomcat::ajp_port} accept - tomcat":
-      port   => $::tomcat::ajp_port,
+      dport  => $::tomcat::ajp_port,
       proto  => 'tcp',
       action => 'accept'
     }
@@ -27,7 +27,7 @@ class tomcat::firewall {
   # ssl connector
   if $::tomcat::ssl_connector {
     firewall { "${::tomcat::ssl_port} accept - tomcat":
-      port   => $::tomcat::ssl_port,
+      dport  => $::tomcat::ssl_port,
       proto  => 'tcp',
       action => 'accept'
     }
@@ -36,7 +36,7 @@ class tomcat::firewall {
   # jmx
   if $::tomcat::jmx_listener {
     firewall { "${::tomcat::jmx_registry_port}/${::tomcat::jmx_server_port} accept - tomcat":
-      port   => [$::tomcat::jmx_registry_port, $::tomcat::jmx_server_port],
+      dport  => [$::tomcat::jmx_registry_port, $::tomcat::jmx_server_port],
       proto  => 'tcp',
       action => 'accept'
     }
