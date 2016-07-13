87d768759950e80fb0ba92976a43d31e24e4069c
Thomas <t.buehler24@gmail.com>
2016-07-13 15:29:01 +0200

    Enhance userdb type in order to add new roles

--- manifests/userdb_entry.pp
+++ manifests/userdb_entry.pp
@@ -1,8 +1,7 @@
-# == Define: tomcat::userdb_entry
-#
 define tomcat::userdb_entry (
-  $username,
-  $password,
+  $add_roles = 'false',
+  $username = undef,
+  $password = undef,
   $roles,
   $database = 'main UserDatabase') {
   # The base class must be included first
@@ -10,16 +9,22 @@ define tomcat::userdb_entry (
     fail('You must include the tomcat base class before using any tomcat defined resources')
   }
 
-  # parameters validation
-  validate_array($roles)
-  validate_string($username, $password)
-
-  $roles_string = join($roles, ',')
-
-  # add formated fragment
-  concat::fragment { "UserDatabase entry (${title})":
+  if $add_roles == 'true' {
+    validate_array($roles)
+    concat::fragment { "UserDatabase entry (${title})":
+      target  => $database,
+      content => template("${module_name}/common/UserDatabase_role_entry.erb"),
+      order   => 2
+    }
+  }
+  elsif $add_roles == 'false' {
+    validate_array($roles)
+    validate_string($username, $password)
+    $roles_string = join($roles, ',')
+    concat::fragment { "UserDatabase entry (${title})":
     target  => $database,
     content => template("${module_name}/common/UserDatabase_entry.erb"),
-    order   => 2
+    order   => 3
+    }
   }
 }
